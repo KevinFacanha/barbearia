c0085144369e138d79b7f39bb7e80a83a38ec366
Kevin Façanha <y0kevin@outlook.com>
2025-03-07 08:49:29 -0300

    Publish settings added 1

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/barbearia/',  
   plugins: [react()],
-  base:'barbearia',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
